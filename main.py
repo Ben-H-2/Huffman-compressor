e5def860b251df10b1a3b405e3ecd9d092e7369a
Ben-H-2 <benjaminhorton141@gmail.com>
2026-06-24 11:09:58 +0000

    implemented probability , character frequency, and entropy functions

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import text_stats as stats
 
+filepath = "sample_texts/repetitive_text.txt"
+with open(filepath, "r") as f:
+        contents = f.read()
+
+print(f"file name is: {filepath}\n")
+frequencies = stats.get_char_frequencies(contents)
+print(f"frequencies of characters are: {frequencies}\n")
+probabilities = stats.get_probabilities(frequencies)
+#print(f"probabilities are: {probabilities:.3f}\n")
+entropy = stats.calculate_entropy(probabilities)
+print(f"Entropy for {filepath} is {entropy:.3f}\n")
+bits_per_char = stats.find_fixed_bit_length_bits_per_char(frequencies)
+print(f"bits per character when fixed bit length: {bits_per_char}")
+total_fixed_bit_size = stats.find_total_fixed_bit_file_size(bits_per_char,contents)
+print(f"therefore total bit size would be: {total_fixed_bit_size}")
--- text_stats.py
+++ text_stats.py
@@ -1,1 +1,27 @@
+from collections import Counter
+import math as m
 
+def get_char_frequencies(contents):
+    return Counter(contents)
+
+def get_probabilities(frequencies):
+    total = sum(frequencies.values())
+    probabilities = {}
+    for char,count in frequencies.items():
+        probabilities[char] = count/total
+    return probabilities
+
+def calculate_entropy(probabilities):
+    entropy = 0
+    for i in probabilities.values():
+        entropy += -1*(i*m.log2(i))
+    return entropy
+
+def find_fixed_bit_length_bits_per_char(frequencies):
+    length = len(frequencies)
+    bits_per_char = int(m.ceil(m.log2(length)))
+    return bits_per_char
+
+def find_total_fixed_bit_file_size(bits_per_char,content):
+    total_size = bits_per_char * len(content)
+    return total_size
